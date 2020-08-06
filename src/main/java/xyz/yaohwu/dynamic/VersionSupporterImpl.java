a89dd35f02405c39a36ef6adcbf0fdd011547887
yaohwu <yaohwu@163.com>
2020-08-06 09:59:42 +0800

    clean code

--- src/main/java/xyz/yaohwu/dynamic/VersionSupporterImpl.java
+++ src/main/java/xyz/yaohwu/dynamic/VersionSupporterImpl.java
@@ -6,10 +6,8 @@ import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.LoadedTypeInitializer;
+import xyz.yaohwu.tool.OutputClassFileContent;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.concurrent.Callable;
 
@@ -48,7 +46,7 @@ public enum VersionSupporterImpl implements VersionSupporter {
                             ClassFileLocator.Simple.of("xyz.yaohwu.dynamic.ActualCallAdvisor", advisorType.getBytes()),
                             ClassFileLocator.ForClassLoader.of(classLoader)
                     );
-            VersionSupporterImpl.outputClassContentToFile("xyz.yaohwu.dynamic.ActualCallAdvisor", advisorType.getBytes());
+            OutputClassFileContent.output("xyz.yaohwu.dynamic.ActualCallAdvisor", advisorType.getBytes());
             advisorTypeDesc = advisorType.getTypeDescription();
             if (advisorTypeDesc == null || fileLocator == null) {
                 System.out.println("try using advisor version failed");
@@ -100,7 +98,7 @@ public enum VersionSupporterImpl implements VersionSupporter {
                             ClassFileLocator.Simple.of("xyz.yaohwu.dynamic.ActualEmptyAdvisor", advisorType.getBytes()),
                             ClassFileLocator.ForClassLoader.of(classLoader)
                     );
-            VersionSupporterImpl.outputClassContentToFile("xyz.yaohwu.dynamic.ActualEmptyAdvisor", advisorType.getBytes());
+            OutputClassFileContent.output("xyz.yaohwu.dynamic.ActualEmptyAdvisor", advisorType.getBytes());
             advisorTypeDesc = advisorType.getTypeDescription();
             if (advisorTypeDesc == null || fileLocator == null) {
                 System.out.println("try using empty advisor version failed");
@@ -116,19 +114,5 @@ public enum VersionSupporterImpl implements VersionSupporter {
             classLoader = ClassLoader.getSystemClassLoader();
         }
         return classLoader;
-    }
-
-    private static void outputClassContentToFile(String name, byte[] bytes) {
-        File file = new File("/Users/yaohwu/finecode/recompile/recompile-code/" + name + "." + "advisor" + ".class");
-        try {
-            if ((file.exists() && file.delete() && file.createNewFile()) || (!file.exists() && file.createNewFile())) {
-                try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
-                    fileOutputStream.write(bytes);
-                    fileOutputStream.flush();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
